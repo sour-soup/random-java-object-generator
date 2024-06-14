75fe965a0a313fc30927051dba3ee2056ef8d806
sour-soup <kiziyepi8@gmail.com>
2024-06-14 21:09:18 +0400

    added lombok and DTO examples

---
+++ src/main/java/org/example/dto/Address.java
@@ -0,0 +1,10 @@
+package org.example.dto;
+
+import lombok.ToString;
+
+@ToString
+public class Address {
+    private String street;
+    private String city;
+    private String zipCode;
+}
---
+++ src/main/java/org/example/dto/User.java
@@ -0,0 +1,10 @@
+package org.example.dto;
+
+import lombok.ToString;
+
+@ToString
+public class User {
+    private String name;
+    private Integer age;
+    private Address address;
+}
